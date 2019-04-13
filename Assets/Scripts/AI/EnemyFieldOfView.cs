ddc3d624adb3b65a6c71857f2464f27ccc80a70b
Slayer <ya-anton3632@yandex.ru>
2019-04-13 10:31:44 +0300

    Basic AI
    -Added 'go to player' script
    -Added 'patrol' script
    -Added 'enemy_field_of_view' script (head player detect trigger)
    -Added simple enemy model (not animated)

---
+++ Assets/Scripts/AI/EnemyFieldOfView.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyFieldOfView : MonoBehaviour
+{
+    public GameObject Enemy;
+    private bool timer_go = false;
+    public float timer;//для потери перса из виду
+    private bool yes = false;
+    private float new_timer;
+    EnemyPatrol EnemyPatrolScript;
+    ToPlayer ToPlayerScript;
+    private bool disable = false;
+    RaycastHit hit;
+
+    void Start()
+    {
+        ToPlayerScript = GameObject.FindWithTag("Enemy").GetComponent<ToPlayer>();
+        EnemyPatrolScript = GameObject.FindWithTag("Enemy").GetComponent<EnemyPatrol>();
+        new_timer = timer;
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        yes = true;
+        Enemy.gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>().enabled = true;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        disable = true;
+
+        Vector3 dir = GetComponent<Transform>().transform.localPosition - transform.localPosition;
+        
+        if (Physics.Raycast(transform.position,dir, 10)
+            && hit.collider.gameObject == GameObject.FindWithTag("Player")) //player object here will be your Player GameObject
+        {
+            if (other.tag == "Player")
+            {
+                timer_go = false;
+                timer = new_timer;
+                yes = true;
+                Enemy.gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>().enabled = true;
+                //сюда анимацию ходьбы
+                //Enemy.GameObject.GetComponent<Animator>().SetTrigger("Walk");
+            }
+        }
+
+        
+        
+    }
+    
+    void OnTriggerExit(Collider other)
+    {
+        yes = false;
+        timer_go = true;
+    }
+    
+    void Update()
+    {
+        RaycastHit hit;
+        Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit,0);
+        if (disable == true)
+        {
+            ToPlayerScript.enabled = true;
+            EnemyPatrolScript.enabled = false;
+        }
+        if (timer_go == true)
+        {
+            timer -= Time.deltaTime;
+        }
+
+        if (timer < 0)
+        {
+            disable = false;
+            timer_go = false;
+           
+            ToPlayerScript.enabled = false;
+            EnemyPatrolScript.enabled = true;
+            Enemy.gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>().ResetPath();
+
+            yes = false;
+            timer = new_timer;
+            //сюда анимацию idle
+            //Enemy.GameObject.GetComponent<Animator>().SetTrigger("Idle");
+        }
+    }
+}
---
+++ Assets/Scripts/AI/ToPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToPlayer : MonoBehaviour
+{
+    public Transform target;
+    UnityEngine.AI.NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        if (agent.enabled != true)
+            agent.enabled = true;
+    }
+
+    void Update()
+    {
+        if(agent.GetComponent<UnityEngine.AI.NavMeshAgent>().enabled == true)
+            agent.SetDestination(target.position);
+    }
+}
