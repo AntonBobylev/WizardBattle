e22c4051c2bd87f45fa8a4d11643492cffb1b324
Slayer <ya-anton3632@yandex.ru>
2019-04-13 13:02:34 +0300

    FIXED PROJECT

---
+++ Assets/Scripts/HealthSystem/EnemyHealth.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float MaxHealth = 100.0f;
+    public float Health = 100.0f;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        AddJustCurrentHealth(0);
+    }
+
+    public void AddJustCurrentHealth(int odj)
+    {
+        Health += odj;
+        if (Health < 1.0f)
+            Health = 0.0f;
+
+        if (Health>MaxHealth)
+            Health = MaxHealth;
+
+        if (MaxHealth < 1.0f)
+            MaxHealth = 1.0f;
+    }
+}
---
+++ Assets/Scripts/HealthSystem/PlayerHealth.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float MaxHealth = 100.0f;
+    public float Health = 100.0f;
+    public float healthBarLength;
+
+    void Start()
+    {
+        healthBarLength = Screen.width / 2;
+    }
+
+    void Update()
+    {
+        AddJustCurrentHealth(0);
+    }
+
+    void OnGUI()
+    {
+        GUI.Box(new Rect(10,10,healthBarLength, 20), Health + "/"+MaxHealth);
+    }
+
+    public void AddJustCurrentHealth(int odj)
+    {
+        Health += odj;
+        if (Health < 1.0f)
+            Health = 0.0f;
+
+        if (Health>MaxHealth)
+            Health = MaxHealth;
+
+        if (MaxHealth < 1.0f)
+            MaxHealth = 1.0f;
+        healthBarLength = (Screen.width / 2) * (Health / (float)MaxHealth);
+    }
+}
